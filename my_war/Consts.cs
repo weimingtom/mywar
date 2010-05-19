fe7254c0f1210cf2a00d03f80ff490550c19b027
konstantin.khukalenko <konstantin.khukalenko@gmail.com>
2010-05-19 17:25:52 +0000

    1. Added game fields grid.

---
+++ my_war/Consts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace my_war
+{
+    class Consts
+    {
+        public const int RESOURCE_LIMIT = 10;
+        public const string BUILDING_ICON = "#";
+    }
+}
--- my_war/MainForm.cs
+++ my_war/MainForm.cs
@@ -13,10 +13,23 @@ namespace my_war
     public partial class MainForm : Form
     {
         private ServiceHost m_host;
+        private int m_usedResource = 0;
+
         public MainForm(ServiceHost _host)
         {
             this.m_host = _host;
             InitializeComponent();
+            InitEmptyGameFields();
+            this.TextBox_ResourceCounter.Text = Consts.RESOURCE_LIMIT.ToString();
+        }
+
+        private void InitEmptyGameFields()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                this.DataGridView_PlayerField.Rows.Add();
+                this.DataGridView_CompetitorField.Rows.Add();
+            }
         }
 
         private void ToolStripMenuItem_CreateServer_Click(object sender, EventArgs e)
@@ -29,6 +42,37 @@ namespace my_war
         {
             ConnectToServerForm form = new ConnectToServerForm();
             form.ShowDialog(this);
+        }
+
+        private void DataGridView_PlayerField_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (this.DataGridView_PlayerField.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != Consts.BUILDING_ICON)
+            {
+                if (m_usedResource < Consts.RESOURCE_LIMIT)
+                {
+                    this.DataGridView_PlayerField.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = Consts.BUILDING_ICON;
+                    m_usedResource++;
+                }
+            }
+            else
+            {
+                this.DataGridView_PlayerField.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = "";
+                m_usedResource--;
+            }
+
+            TextBox_ResourceCounter.Text = (Consts.RESOURCE_LIMIT - m_usedResource).ToString();
+        }
+
+        private void TextBox_ResourceCounter_TextChanged(object sender, EventArgs e)
+        {
+            if (TextBox_ResourceCounter.Text == "0")
+            {
+                TextBox_ResourceCounter.BackColor = Color.Red;
+            }
+            else
+            {
+                TextBox_ResourceCounter.BackColor = Color.White;
+            }
         }
     }
 }
